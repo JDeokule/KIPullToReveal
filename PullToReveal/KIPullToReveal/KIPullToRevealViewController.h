4fcf0a53f27f14efbb9d219d63ea3a30a3f3ce63
Jon Deokule <JDeokule@gmail.com>
2013-09-27 13:00:43 -0400

    lots of cleanup. configurable height floor

--- PullToReveal/KIPullToReveal/KIPullToRevealViewController.h
+++ PullToReveal/KIPullToReveal/KIPullToRevealViewController.h
@@ -2,21 +2,10 @@
 //  PullToRevealViewController.h
 //  PullToReveal
 //
-//  Created by Marcus Kida on 02.11.12.
-//  Copyright (c) 2012 Marcus Kida. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import "KIPullToRevealCell.h"
-
-typedef enum {
-	/** Middle bar is shown using a UITextField. This is the default. */
-	KIPullToRevealModeSearch,
-    KIPullToRevealModeMiddleView,
-	KIPullToRevealModeNone
-} KIPullToRevealMode;
-
 
 @protocol KIPullToRevealDelegate <NSObject>
 
@@ -25,20 +14,16 @@ typedef enum {
 - (id<MKAnnotation>) annotationForIndex:(NSUInteger)index;
 
 @optional
-- (void) pullToRevealDidSearchFor:(NSString *)searchText;
 - (void) didSelectAnnotation:(id<MKAnnotation>)annotation;
 
 @end
 
 @interface KIPullToRevealViewController : UITableViewController<MKMapViewDelegate>
 
-@property (nonatomic) KIPullToRevealMode mode;
 
 @property (nonatomic, strong) id <KIPullToRevealDelegate> pullToRevealDelegate;
 @property (nonatomic, assign) BOOL centerUserLocation;
 @property (nonatomic, retain) MKMapView *mapView;
-
-@property (nonatomic, retain) UIToolbar *toolbar;
 
 @property (nonatomic, retain) UIView *middleView;
 @property (nonatomic, retain) UILabel *middleViewLabel;
--- PullToReveal/KIPullToReveal/KIPullToRevealViewController.m
+++ PullToReveal/KIPullToReveal/KIPullToRevealViewController.m
@@ -2,12 +2,9 @@
 //  PullToRevealViewController.m
 //  PullToReveal
 //
-//  Created by Marcus Kida on 02.11.12.
-//  Copyright (c) 2012 Marcus Kida. All rights reserved.
-//
 
-#define kKIPTRTableViewContentInsetX     200.0f
 #define kKIPTRAnimationDuration          0.5f
+#define defaultContentInset 390.0f
 
 #import "KIPullToRevealViewController.h"
 #import <QuartzCore/QuartzCore.h>
@@ -16,11 +13,12 @@
 @interface KIPullToRevealViewController () <UIScrollViewDelegate, UITextFieldDelegate, MKMapViewDelegate>
 {
     @private
-    UITextField *_searchTextField;
     BOOL _scrollViewIsDraggedDownwards;
     double _lastDragOffset;
     int middleViewHeight;
 }
+
+@property (nonatomic) CGFloat tableViewContentInsetX;
 @end
 
 @implementation KIPullToRevealViewController
@@ -28,20 +26,44 @@
 @synthesize pullToRevealDelegate = _pullToRevealDelegate;
 @synthesize centerUserLocation = _centerUserLocation;
 @synthesize mapView = _mapView;
-@synthesize toolbar = _toolbar;
-//@synthesize middleView = _middleView;
-//@synthesize middleViewLabel = _middleViewLabel;
-//@synthesize middleViewImageView = _middleViewImageView;
-@synthesize mode = _mode;
 @synthesize pinSelectionEnabled = _pinSelectionEnabled;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
-    self = [super initWithStyle:style];
-    if (self) {
-        middleViewHeight = 30;
+    if (self = [super initWithStyle:style]) {
+        [self commonInitMethod];
     }
     return self;
+}
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self commonInitMethod];
+    }
+    return self;
+}
+
+- (id) init
+{
+    if (self = [super init]) {
+        [self commonInitMethod];
+    }
+    return self;
+}
+
+- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        [self commonInitMethod];
+    }
+    return self;
+}
+
+- (void) commonInitMethod
+{
+    middleViewHeight = 30;
+    _tableViewContentInsetX = defaultContentInset;
 }
 
 - (void)viewDidLoad
@@ -49,25 +71,15 @@
     [super viewDidLoad];
 
     [self initializeMapView];
-}
 
-- (void) viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-
-    if (self.mode == KIPullToRevealModeMiddleView ) {
-        middleViewHeight = 30;
-//        [self initializeView];
-    } else {
-        middleViewHeight = 44;
-        [self initalizeToolbar];
-    }
+    middleViewHeight = 30;
+    _tableViewContentInsetX = .7 * self.view.frame.size.height;;
 }
 
 - (void) viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    [self displayMapViewAnnotationsForTableViewCells];
+    [self displayMapViewAnnotationsFromDelegate];
 }
 
 - (void)didReceiveMemoryWarning
@@ -76,22 +88,18 @@
 }
 
 #pragma mark - Private methods
-- (void) initializeView
-{
-}
-
 - (void) setPinSelection:(BOOL)enabled
 {
     if (_pinSelectionEnabled != enabled) {
         _pinSelectionEnabled = enabled;
-        [self performSelectorOnMainThread:@selector(displayMapViewAnnotationsForTableViewCells) withObject:nil waitUntilDone:YES];
+        [self performSelectorOnMainThread:@selector(displayMapViewAnnotationsFromDelegate) withObject:nil waitUntilDone:YES];
     }
 }
 
 - (void) initializeMapView
 {
     if (!_mapView) {
-        [self.tableView setContentInset:UIEdgeInsetsMake(kKIPTRTableViewContentInsetX,0,0,0)];
+        [self.tableView setContentInset:UIEdgeInsetsMake(self.tableViewContentInsetX,0,0,0)];
         _mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0, self.tableView.contentInset.top*-1, self.tableView.bounds.size.width, self.tableView.contentInset.top)];
         [_mapView setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
         [_mapView setShowsUserLocation:NO];
@@ -110,27 +118,6 @@
     }
 }
 
-- (void) initalizeToolbar
-{
-    if (!_toolbar) {
-        _toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, -middleViewHeight, self.tableView.bounds.size.width, middleViewHeight)];
-        [_toolbar setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
-        
-        if (self.mode == KIPullToRevealModeSearch) {
-            _searchTextField = [[UITextField alloc] initWithFrame:CGRectMake(10, 7, _toolbar.bounds.size.width-20, 30)];
-            [_searchTextField setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
-            [_searchTextField setBorderStyle:UITextBorderStyleRoundedRect];
-            [_searchTextField setReturnKeyType:UIReturnKeySearch];
-            [_searchTextField setClearButtonMode:UITextFieldViewModeWhileEditing];
-            [_searchTextField addTarget:self action:@selector(searchTextFieldBecomeFirstResponder:) forControlEvents:UIControlEventEditingDidBegin];
-            [_searchTextField setDelegate:self];
-            [_toolbar addSubview:_searchTextField];
-        }
-
-        [self.tableView insertSubview:_toolbar aboveSubview:self.tableView];
-    }
-}
-
 - (void) centerToUserLocation
 {
     [_mapView setCenterCoordinate:_mapView.userLocation.coordinate animated:YES];
@@ -145,33 +132,31 @@
     [_mapView setRegion:mapRegion animated: YES];
 }
 
-
 #pragma mark - ScrollView Delegate
 - (void) scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
 {
     double contentOffset = scrollView.contentOffset.y;
     _lastDragOffset = contentOffset;
 
-    if(contentOffset < kKIPTRTableViewContentInsetX*-1)
+    if(contentOffset < -self.tableViewContentInsetX)
     {
         [self zoomMapToFitAnnotations];
         [self setPinSelection:YES];
-        
         [UIView animateWithDuration:kKIPTRAnimationDuration
                          animations:^()
          {
-             [self.tableView setContentInset:UIEdgeInsetsMake(self.tableView.bounds.size.height,0,0,0)];
+             [self.tableView setContentInset:UIEdgeInsetsMake(self.tableViewContentInsetX,0,0,0)];
              [self.tableView scrollsToTop];
          }];
     }
-    else if (contentOffset >= kKIPTRTableViewContentInsetX*-1)
+    else if (contentOffset >= -self.tableViewContentInsetX)
     {
         [self setPinSelection:NO];
         
         [UIView animateWithDuration:kKIPTRAnimationDuration
                          animations:^()
          {
-             [self.tableView setContentInset:UIEdgeInsetsMake(kKIPTRTableViewContentInsetX,0,0,0)];
+             [self.tableView setContentInset:UIEdgeInsetsMake(self.tableViewContentInsetX,0,0,0)];
 
          }];
         
@@ -201,7 +186,7 @@
          ];
         [self setPinSelection:NO];
 
-        [self.tableView setContentInset:UIEdgeInsetsMake(kKIPTRTableViewContentInsetX,0,0,0)];
+        [self.tableView setContentInset:UIEdgeInsetsMake(self.tableViewContentInsetX,0,0,0)];
         
         if(_centerUserLocation)
         {
@@ -212,18 +197,7 @@
         [self.tableView scrollsToTop];
     }
 
-    if(contentOffset >= -middleViewHeight)
-    {
-//        [_toolbar removeFromSuperview];
-//        [_toolbar setFrame:CGRectMake(0, contentOffset, self.tableView.bounds.size.width, middleViewHeight)];
-//        [self.tableView addSubview:_toolbar];
-    }
-    else if(contentOffset < 0)
-    {
-//        [_toolbar removeFromSuperview];
-//        [_toolbar setFrame:CGRectMake(0, -middleViewHeight, self.tableView.bounds.size.width, middleViewHeight)];
-//        [self.tableView insertSubview:_toolbar aboveSubview:self.tableView];
-
+    if(contentOffset < 0) {
         // Resize map to viewable size
         [_mapView setFrame:
          CGRectMake(0, self.tableView.bounds.origin.y, self.tableView.bounds.size.width, contentOffset*-1)
@@ -235,44 +209,13 @@
     {
         [self centerToUserLocation];
         [self zoomToUserLocation];
-        [self displayMapViewAnnotationsForTableViewCells];
+        [self displayMapViewAnnotationsFromDelegate];
     }
 }
 
-#pragma mark - TextField Delegate
-- (BOOL) textFieldShouldReturn:(UITextField *)textField
-{
-    if(_pullToRevealDelegate && [_pullToRevealDelegate respondsToSelector:@selector(PullToRevealDidSearchFor:)])
-        [[self pullToRevealDelegate] pullToRevealDidSearchFor:textField.text];
-    
-    [_searchTextField resignFirstResponder];
-    return YES;
-}
 
-#pragma mark - SearchTextField
-- (void) searchTextFieldBecomeFirstResponder: (id)sender
-{
-    [UIView animateWithDuration:kKIPTRAnimationDuration
-                     animations:^()
-     {
-         [self.tableView setContentInset:UIEdgeInsetsMake(kKIPTRTableViewContentInsetX+middleViewHeight,0,0,0)];
-         [_mapView setFrame:
-          CGRectMake(0, self.tableView.contentInset.top*-1, self.tableView.bounds.size.width, self.tableView.contentInset.top)
-          ];
-         [self setPinSelection:NO];
-         
-         if(_centerUserLocation)
-         {
-             [self centerToUserLocation];
-             [self zoomToUserLocation];
-         }
-         
-         [self.tableView scrollsToTop];
-     }];
-    [_searchTextField becomeFirstResponder];
-}
 #pragma mark - MapView
-- (void) displayMapViewAnnotationsForTableViewCells
+- (void) displayMapViewAnnotationsFromDelegate
 {
     [_mapView removeAnnotations:_mapView.annotations];
 
